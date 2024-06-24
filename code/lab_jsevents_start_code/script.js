add7c9553f22fa82dd14fd07a483fd71b3a2aa98
Gellila Zeru <gellilazeru1@gmail.com>
2024-06-24 16:09:03 +0100

    started extension and created complete button

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -1,7 +1,8 @@
 //Selectors
 
 const enterButton = document.querySelector("#enter"); 
-const list = document.querySelector("#list"); 
+const list = document.querySelector("#list");
+const completedList = document.querySelector("#completed-list") 
 const textInput = document.querySelector("#new-todo"); 
 const textOutput = document.querySelector("#text-output");
 const addToDo = document.querySelector("#todo-form");
@@ -10,17 +11,20 @@ const dateDisplay = document.querySelector("#date-display");
 
 //Logic
 
-textInput.addEventListener("input", (event) => {
-    textOutput.innerText = event.target.value; 
-})
+// textInput.addEventListener("input", (event) => {
+//     textOutput.innerText = event.target.value; 
+// })
 
-enterButton.addEventListener("click", () => {
-    console.log("button clicked");
+// enterButton.addEventListener("click", () => {
+//     console.log("button clicked");
 
-});
+// });
 
-addToDo.addEventListener("submit", (event) => {
+addToDo.addEventListener("submit", formSubmit);
+
+function formSubmit (event) {
     event.preventDefault();
+<<<<<<< HEAD
     const newItem = document.createElement("li");
     newItem.innerText = event.target[0].value;
     list.appendChild(newItem); 
@@ -31,8 +35,62 @@ addToDo.addEventListener("submit", (event) => {
         list.removeChild(newItem);
     });
     newItem.appendChild(deleteButton);
+=======
+    const newItem = createToDoItem(textInput.value);
+>>>>>>> 84e223a (started extension and created complete button)
     list.appendChild(newItem);
-});
+}
+
+
+function createToDoItem (text) {
+
+    addToDo.addEventListener("submit", (event) => {
+        event.preventDefault();
+    
+        const newItem = document.createElement("li");
+        newItem.innerText = event.target[0].value;
+        list.appendChild(newItem); 
+
+        const completeButton = createButton ("complete", completedItems);
+        completeButton.textContent = "Completed";
+        newItem.appendChild(completeButton);
+        
+        const deleteButton = createButton("delete", canDeleteToDoItem);
+        deleteButton.textContent = "Delete";
+        newItem.appendChild(deleteButton);
+
+    //     deleteButton.addEventListener("click", () => {
+    //         list.removeChild(newItem);
+    //     });
+    //     newItem.appendChild(deleteButton);
+    //     list.appendChild(newItem);
+    });
+}
+
+function canDeleteToDoItem (event) {
+    const item = event.target[0].value;
+    item.removeChild(newItem);
+
+}
+
+// extension
+
+function completedItems (event) {
+    const item = event.target[0].value;
+    item.classList.add("completed");
+}
+
+function createButton (text, onClick) {
+    
+    const button = document.createElement("button");
+    button.textContent = text;
+    button.addEventListener("click", onClick);
+    return button;
+}
+
+
+
+
 
 showDateButton.addEventListener("click", () => {
     const currentDate = new Date(); 
