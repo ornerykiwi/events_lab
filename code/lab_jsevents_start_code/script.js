f086825eb585904cac8547b3e0611df9e88d28c2
Charisma <actuallycharisma@outlook.com>
2024-06-24 14:38:23 +0100

    added initial selectors and button logic

---
+++ code/lab_jsevents_start_code/script.js
@@ -0,0 +1,27 @@
+//Selectors
+
+const enterButton = document.querySelector("#enter"); 
+const list = document.querySelector("#list"); 
+const textInput = document.querySelector("#new-todo"); 
+const textOutput = document.querySelector("#text-output")
+const addToDo = document.querySelector("#todo-form")
+
+//Logic
+
+textInput.addEventListener("input", (event) => {
+    textOutput.innerText = event.target.value; 
+})
+
+enterButton.addEventListener("click", () => {
+    console.log("button clicked");
+
+});
+
+addToDo.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const newItem = document.createElement("li");
+    newItem.innerText = event.target.value;
+    list.appendChild(newItem); 
+
+})
+
